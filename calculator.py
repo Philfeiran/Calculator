59d611f8283dc0973f789ecfc4b6b619810b4e07
Philfeiran <51011052+Philfeiran@users.noreply.github.com>
2021-03-18 08:56:17 -0400

    task: create calculator class with all functions

--- calculator.py
+++ calculator.py
@@ -1,1 +1,24 @@
+import  math
+class calculator:
+    def __init__(self):
+        self.result =0
 
+    def add(self,x,y):
+        result = x+y
+        return result
+    def minus(self,x,y):
+        result =x-y
+        return result
+    def multply(self,x,y):
+        result = x*y
+        return result
+    def divide(self,x,y):
+        result = x/y
+        return result
+
+    def square(self,x):
+        result = x*x
+        return result
+    def squareRoot(self,x):
+        result=math.sqrt(x)
+        return result
