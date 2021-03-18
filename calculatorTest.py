3a1b856db1c8e864d392a12f8f6d8d3a136f4078
Philfeiran <51011052+Philfeiran@users.noreply.github.com>
2021-03-18 08:58:30 -0400

    task: build unittest class with 6 test cases

--- calculatorTest.py
+++ calculatorTest.py
@@ -1,1 +1,80 @@
 
+from calculator import calculator
+import unittest
+import csv
+
+class testCalculator(unittest.TestCase):
+
+    def testAdd(self):
+        cal = calculator()
+        csvFile = open("Unit Test Addition.csv", "r")
+        reader = csv.reader(csvFile)
+        for item in reader:
+            if reader.line_num ==1:
+                continue
+            x=int(item[0])
+            y=int(item[1])
+            result=int(item[2])
+            self.assertEqual(cal.add(x,y),result)
+    def testMinus(self):
+        cal = calculator()
+        csvFile = open("Unit Test Subtraction.csv", "r")
+        reader = csv.reader(csvFile)
+        for item in reader:
+            if reader.line_num ==1:
+                continue
+            x=int(item[0])
+            y=int(item[1])
+            result=int(item[2])
+            self.assertEqual(cal.minus(y,x),result)
+
+    def testMultip(self):
+        cal = calculator()
+        csvFile = open("Unit Test Multiplication.csv", "r")
+        reader = csv.reader(csvFile)
+        for item in reader:
+            if reader.line_num == 1:
+                continue
+            x = int(item[0])
+            y = int(item[1])
+            result = int(item[2])
+            self.assertEqual(cal.multply(y, x), result)
+
+    def testDivide(self):
+        cal = calculator()
+        csvFile = open("Unit Test Division.csv", "r")
+        reader = csv.reader(csvFile)
+        for item in reader:
+            if reader.line_num == 1:
+                continue
+            x = int(item[0])
+            y = int(item[1])
+            result = float(item[2])
+            self.assertEqual(round(cal.divide(y, x),5), round(result,5))
+
+    def testSquare(self):
+        cal = calculator()
+        csvFile = open("Unit Test Square.csv", "r")
+        reader = csv.reader(csvFile)
+        for item in reader:
+            if reader.line_num == 1:
+                continue
+            x = int(item[0])
+            result = int(item[1])
+            self.assertEqual(cal.square(x), result)
+
+
+    def testSquareRoot(self):
+        cal = calculator()
+        csvFile = open("Unit Test Square Root.csv", "r")
+        reader = csv.reader(csvFile)
+        for item in reader:
+            if reader.line_num == 1:
+                continue
+            x = int(item[0])
+            result = float(item[1])
+            self.assertEqual(round(cal.squareRoot(x),5), round(result,5))
+
+if __name__ == '__main__':
+
+    unittest.main()
