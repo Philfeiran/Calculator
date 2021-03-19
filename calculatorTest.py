8098d943c237cbe736b5af28f70a638d6d62cc3e
Philfeiran <51011052+Philfeiran@users.noreply.github.com>
2021-03-19 18:03:51 -0400

    bug: remove repeated code

--- calculatorTest.py
+++ calculatorTest.py
@@ -2,77 +2,65 @@ from calculator import calculator
 import unittest
 import csv
 
+
+def readFile (file):
+    csvFile = open(file, "r")
+    reader = csv.reader(csvFile)
+    IsThreeVar = True
+    x=[]
+    y=[]
+    result=[]
+    for item in reader:
+        if reader.line_num==1:
+            if item[2]==' ':
+                IsThreeVar=False
+            continue
+        x.append(float(item[0]))
+        if not IsThreeVar:
+            result.append(float(item[1]))
+        else:
+            y.append(float(item[1]))
+            result.append(float(item[2]))
+
+    if IsThreeVar:
+        return x,y,result
+    else:
+        return x,result
+
 class testCalculator(unittest.TestCase):
-
-    def testAdd(self):
-        cal = calculator()
-        csvFile = open("Unit Test Addition.csv", "r")
-        reader = csv.reader(csvFile)
-        for item in reader:
-            if reader.line_num ==1:
-                continue
-            x=float(item[0])
-            y=float(item[1])
-            result=float(item[2])
-            self.assertEqual(cal.add(x,y),result)
-    def testMinus(self):
-        cal = calculator()
-        csvFile = open("Unit Test Subtraction.csv", "r")
-        reader = csv.reader(csvFile)
-        for item in reader:
-            if reader.line_num ==1:
-                continue
-            x=float(item[0])
-            y=float(item[1])
-            result=float(item[2])
-            self.assertEqual(cal.minus(y,x),result)
-
-    def testMultip(self):
-        cal = calculator()
-        csvFile = open("Unit Test Multiplication.csv", "r")
-        reader = csv.reader(csvFile)
-        for item in reader:
-            if reader.line_num == 1:
-                continue
-            x = float(item[0])
-            y = float(item[1])
-            result = float(item[2])
-            self.assertEqual(cal.multply(y, x), result)
-
-    def testDivide(self):
-        cal = calculator()
-        csvFile = open("Unit Test Division.csv", "r")
-        reader = csv.reader(csvFile)
-        for item in reader:
-            if reader.line_num == 1:
-                continue
-            x = float(item[0])
-            y = float(item[1])
-            result = float(item[2])
-            self.assertEqual(round(cal.divide(y, x),5), round(result,5))
-
+    cal = calculator()
+    def testAddition(self):
+        self.assertEqual(self.cal.add(2,2),4)
+    def testSubtraction(self):
+        self.assertEqual(self.cal.minus(2,2),0)
+    def testMultiplication(self):
+        self.assertEqual(self.cal.multply(2,2),4)
+    def testDivision(self):
+        self.assertEqual(self.cal.divide(2,4),2)
     def testSquare(self):
-        cal = calculator()
-        csvFile = open("Unit Test Square.csv", "r")
-        reader = csv.reader(csvFile)
-        for item in reader:
-            if reader.line_num == 1:
-                continue
-            x = float(item[0])
-            result = float(item[1])
-            self.assertEqual(cal.square(x), result)
-
-
+        self.assertEqual(self.cal.square(4),16)
     def testSquareRoot(self):
-        cal = calculator()
-        csvFile = open("Unit Test Square Root.csv", "r")
-        reader = csv.reader(csvFile)
-        for item in reader:
-            if reader.line_num == 1:
-                continue
-            x = int(item[0])
-            result = float(item[1])
-            self.assertEqual(round(cal.squareRoot(x),5), round(result,5))
+        self.assertEqual(self.cal.squareRoot(4),2)
+    def testCSVfiles(self):
+        #test addition file
+        xADD,yADD,resultADD = readFile("Unit Test Addition.csv")
+        xSUB,ySUB,resultSUB= readFile("Unit Test Subtraction.csv")
+        xMUL,yMUL,resultMUL=readFile("Unit Test Multiplication.csv")
+        xDIV, yDIV, resultDIV = readFile("Unit Test Division.csv")
+        xSQU, resultSQU = readFile("Unit Test Square.csv")
+        xSQR, resultSQR = readFile("Unit Test Square Root.csv")
+
+        #All CSV files have 18 exapmle
+        for i in range (18):
+            self.assertEqual(self.cal.add(xADD[i],yADD[i]),resultADD[i])
+            self.assertEqual(self.cal.minus(xSUB[i],ySUB[i]),resultSUB[i])
+            self.assertEqual(self.cal.multply(xMUL[i],yMUL[i]),resultMUL[i])
+            #for divison and square root, I only campare 6 demicial places
+            self.assertEqual(round(self.cal.divide(xDIV[i],yDIV[i]),6),round(resultDIV[i],6))
+            self.assertEqual(self.cal.square(xSQU[i]),resultSQU[i])
+            self.assertEqual(round(self.cal.squareRoot(xSQR[i]),6),round(resultSQR[i],6))
+
+
 
 if __name__ == '__main__':
 
